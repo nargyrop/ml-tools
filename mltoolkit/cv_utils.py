aaeaf90e754d56a3a317fc21880da7685d6846b2
nikos.argyropoulos <n.argyropoulos@oilx.co>
2022-12-06 17:08:22 +0000

    improvements

--- mltoolkit/cv_utils.py
+++ mltoolkit/cv_utils.py
@@ -5,15 +5,15 @@ import numpy as np
 
 
 def image_generator(
-    X: Sequence,
+    x: Sequence,
     y: Sequence,
-    ids: Sequence,
-    batch_size: int,
+    ids: Sequence = None,
+    batch_size: int = 2,
     clip_pixels: Sequence = None,
-    min_values: Union[float, int] = None,
-    max_values: Union[float, int] = None,
-    mean_values: Union[float, int] = None,
-    std_values: Union[float, int] = None,
+    min_values: Union[float, int] = [],
+    max_values: Union[float, int] = [],
+    mean_values: Union[float, int] = [],
+    std_values: Union[float, int] = [],
     normalize_labels: bool = False,
     random_rotations: bool = False,
     random_flips: bool = False,
@@ -24,7 +24,8 @@ def image_generator(
         X (Sequence): Training data.
         y (Sequence): Training labels.
         ids (Sequence): IDs out which random selections will be drawn.
-        batch_size (int): Batch size.
+        Defaults to None.
+        batch_size (int): Batch size. Defaults to 2.
         clip_pixels (Sequence, optional): Number of pixels to clip. Can be 
         used to match output of a CNN with no padding. Defaults to [].
         min_values (Union[float, int], optional): Min values to be used for 
@@ -37,25 +38,24 @@ def image_generator(
         be used for standardization. Defaults to [].
         normalize_labels (bool, optional): Flag to enable normalization of 
         labels. Defaults to False.
+        random_rotations (bool, optional): Flag to enable randomly rotating the samples in multiples of 90deg.
+        Defaults to False.
+        random_flips (bool, optional): Flag to enable randomly flipping the samples.
+        Defaults to False.
 
     Yields:
         Iterator[Sequence[np.ndarray, np.ndarray]]: Batch of data
     """
-    if isinstance(min_values, Iterable):
-        min_values = np.array(min_values)
-    if isinstance(max_values, Iterable):
-        max_values = np.array(max_values)
-    if isinstance(mean_values, Iterable):
-        mean_values = np.array(mean_values)
-    if isinstance(std_values, Iterable):
-        std_values = np.array(std_values)
-    
+
+    if ids is None:
+        ids = list(range(len(x)))
+
     while True:
         # Get the samples you'll use in this batch
         batch_samples = np.random.choice(ids, size=batch_size)
 
         # Initialise X_train and y_train arrays for this batch
-        X_train = X[batch_samples]
+        x_train = x[batch_samples]
         y_train = y[batch_samples]
 
         if clip_pixels:
@@ -67,10 +67,10 @@ def image_generator(
                     ]
 
         # Normalize or standardize data
-        if max_values is not None:
-            if min_values is not None:
-                X_train = np.divide(
-                    X_train - min_values,
+        if any(max_values):
+            if any(min_values):
+                x_train = np.divide(
+                    x_train - min_values,
                     max_values - min_values
                     )
                 if normalize_labels:
@@ -79,12 +79,12 @@ def image_generator(
                         max_values - min_values
                         )
             else:
-                X_train = np.divide(X_train, max_values)
+                x_train = np.divide(x_train, max_values)
                 if normalize_labels:
                     y_train = np.divide(y_train, max_values)
 
-        elif mean_values is not None and std_values is not None:
-            X_train = np.divide(X_train - mean_values, std_values)
+        elif any(mean_values) and any(std_values):
+            x_train = np.divide(x_train - mean_values, std_values)
 
             if normalize_labels:
                 y_train = np.divide(y_train - mean_values, std_values)
@@ -93,14 +93,14 @@ def image_generator(
         if random_rotations:
             n_rotations = np.random.randint(0, 4)
             if n_rotations:
-                X_train = np.rot90(X_train, k=n_rotations, axes=(1, 2))
+                x_train = np.rot90(x_train, k=n_rotations, axes=(1, 2))
                 y_train = np.rot90(y_train, k=n_rotations, axes=(1, 2))
         
         # Random flips
         if random_flips:
             flip = np.random.randint(0, 3)
             if flip:
-                X_train = np.flip(X_train, axis=flip)
+                x_train = np.flip(x_train, axis=flip)
                 y_train = np.flip(y_train, axis=flip)
 
         # Make sure they're numpy arrays (as opposed to lists)
@@ -108,4 +108,4 @@ def image_generator(
         # y_train = np.expand_dims(np.array(y_train), axis=-1)
 
         # yield the next training batch after converting to float32
-        yield X_train.astype(np.float32), y_train.astype(np.float32)
+        yield x_train.astype(np.float32), y_train.astype(np.float32)
---
+++ mltoolkit/metrics.py
@@ -0,0 +1,103 @@
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+
+    import numpy as np
+    import tensorflow as tf
+    import tensorflow.keras.backend as K
+
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+
+def jaccard_distance(y_true, y_pred, smooth=100):
+    """ Calculates mean of Jaccard distance as a loss function """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+
+def dice_loss(y_true, y_pred):
+    numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=(1, 2, 3))
+    denominator = tf.reduce_sum(y_true + y_pred, axis=(1, 2, 3))
+
+    return 1 - numerator / denominator
+
+
+def weighted_binary_cross_entropy(weights: dict, from_logits: bool = False):
+    """
+    Return a function for calculating weighted binary cross entropy
+    It should be used for multi-hot encoded labels
+
+    # Example
+    y_true = tf.convert_to_tensor([1, 0, 0, 0, 0, 0], dtype=tf.int64)
+    y_pred = tf.convert_to_tensor([0.6, 0.1, 0.1, 0.9, 0.1, 0.], dtype=tf.float32)
+    weights = {
+        0: 1.,
+        1: 2.
+    }
+    # with weights
+    loss_fn = get_loss_for_multilabels(weights=weights, from_logits=False)
+    loss = loss_fn(y_true, y_pred)
+    print(loss)
+    # tf.Tensor(0.6067193, shape=(), dtype=float32)
+
+    # without weights
+    loss_fn = get_loss_for_multilabels()
+    loss = loss_fn(y_true, y_pred)
+    print(loss)
+    # tf.Tensor(0.52158177, shape=(), dtype=float32)
+
+    # Another example
+    y_true = tf.convert_to_tensor([[0., 1.], [0., 0.]], dtype=tf.float32)
+    y_pred = tf.convert_to_tensor([[0.6, 0.4], [0.4, 0.6]], dtype=tf.float32)
+    weights = {
+        0: 1.,
+        1: 2.
+    }
+    # with weights
+    loss_fn = get_loss_for_multilabels(weights=weights, from_logits=False)
+    loss = loss_fn(y_true, y_pred)
+    print(loss)
+    # tf.Tensor(1.0439969, shape=(), dtype=float32)
+
+    # without weights
+    loss_fn = get_loss_for_multilabels()
+    loss = loss_fn(y_true, y_pred)
+    print(loss)
+    # tf.Tensor(0.81492424, shape=(), dtype=float32)
+
+    @param weights A dict setting weights for 0 and 1 label. e.g.
+        {
+            0: 1.
+            1: 8.
+        }
+        For this case, we want to emphasise those true (1) label,
+        because we have many false (0) label. e.g.
+            [
+                [0 1 0 0 0 0 0 0 0 1]
+                [0 0 0 0 1 0 0 0 0 0]
+                [0 0 0 0 1 0 0 0 0 0]
+            ]
+
+
+
+    @param from_logits If False, we apply sigmoid to each logit
+    @return A function to calculate (weighted) binary cross entropy
+    """
+    assert 0 in weights
+    assert 1 in weights
+
+    def weighted_cross_entropy_fn(y_true, y_pred):
+        tf_y_true = tf.cast(y_true, dtype=y_pred.dtype)
+        tf_y_pred = tf.cast(y_pred, dtype=y_pred.dtype)
+
+        weights_v = tf.where(tf.equal(tf_y_true, 1), weights[1], weights[0])
+        ce = K.binary_crossentropy(tf_y_true, tf_y_pred, from_logits=from_logits)
+        weights_v = tf.cast(weights_v, dtype=y_pred.dtype)
+        loss = K.mean(tf.multiply(ce, weights_v))
+        return loss
+
+    return weighted_cross_entropy_fn
